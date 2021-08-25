a57e4eb0c10a8e6fdbff13d8488e44460f0c4892
Mark Voltaire <markjvoltaire@gmail.com>
2021-08-25 17:07:20 -0400

    button addition

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("hello")
+
+const updateButton = document.getElementById('update-button');
+const textbox = document.getElementById('text-box');
+console.log ( updateButton, textbox)
+const name = document.getElementById('name');
+updateButton.addEventListener('click', () => {
+    
+    console.log(textbox)
+    // code in here will run with the button is clicked
+
+}); 
+
+
+
